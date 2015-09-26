5e8da5ca57470945f3cae1fbea4800ca59f529a7
Dylan McDonald <dmcdonald21@cnm.edu>
2015-09-25 21:48:53 -0600

    Added get all profiles for the controller

--- public/js/controllers/profile-controller.js
+++ public/js/controllers/profile-controller.js
@@ -1,3 +1,15 @@
-app.controller("ProfileController", ["$scope", function($scope) {
+app.controller("ProfileController", ["$scope", "ProfileService", function($scope, ProfileService) {
 	$scope.tagline = "This is the profile controller";
+	$scope.profiles = [];
+
+	$scope.getProfiles = function() {
+		ProfileService.all()
+			.then(function(result) {
+				if(result.data.status === 200) {
+					$scope.profiles = result.data.data;
+				}
+			});
+	};
+
+	$scope.getProfiles();
 }]);
