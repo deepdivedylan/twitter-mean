63da9c0830bf2d9924b675fd7a3cd81a0b1b917e
Dylan McDonald <dmcdonald21@cnm.edu>
2015-09-25 22:51:23 -0600

    Added the ability to add profiles

--- public/js/controllers/profile-controller.js
+++ public/js/controllers/profile-controller.js
@@ -1,6 +1,18 @@
 app.controller("ProfileController", ["$scope", "ProfileService", function($scope, ProfileService) {
+	$scope.alerts = [];
+	$scope.editedProfile = null;
+	$scope.newProfile = null;
+	$scope.isEditing = false;
 	$scope.tagline = "This is the profile controller";
 	$scope.profiles = [];
+
+	$scope.addAlert = function(result) {
+		var type = "danger";
+		if(result.data.status === 200) {
+			type = "success";
+		}
+		$scope.alerts.push({"type": type, "msg": result.data.message});
+	};
 
 	$scope.getProfiles = function() {
 		ProfileService.all()
@@ -9,6 +21,20 @@ app.controller("ProfileController", ["$scope", "ProfileService", function($scope
 					$scope.profiles = result.data.data;
 				}
 			});
+	};
+
+	$scope.createProfile = function(profile, validated) {
+		if(validated === true) {
+			ProfileService.create(profile)
+				.then(function(result) {
+					$scope.addAlert(result);
+					$scope.getProfiles();
+				});
+		}
+	};
+
+	$scope.closeAlert = function(index) {
+		$scope.alerts.splice(index, 1);
 	};
 
 	$scope.getProfiles();
