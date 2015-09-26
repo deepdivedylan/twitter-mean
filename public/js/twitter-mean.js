a120891808afdbc21c2da1a81cf0482d1a88350f
Dylan McDonald <dmcdonald21@cnm.edu>
2015-09-25 19:16:24 -0600

    Added routing to the main application

--- public/js/twitter-mean.js
+++ public/js/twitter-mean.js
@@ -1,1 +1,18 @@
 var app = angular.module("TwitterMean", ["ngMessages", "ngRoute", "ngSanitize", "ui.bootstrap"]);
+
+app.config(function($locationProvider, $routeProvider) {
+	$routeProvider
+		// home page
+		.when("/", {
+			templateUrl: "views/home.html",
+			controller: "MainController"
+		})
+
+		// profile page that will use the ProfileController
+		.when("/profile", {
+			templateUrl: "views/profile.html",
+			controller: "ProfileController"
+		});
+
+	$locationProvider.html5Mode(true);
+});
